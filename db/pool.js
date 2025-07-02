be0f1ca5acf5a8fbd9b2c5a29985cf31c6caf57b
Gabriel <gabripugas@gmail.com>
2025-07-02 00:33:43 -0300

    git ignore

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,3 @@
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  user: 'postgres',
-  host: 'painel.gabrielpugas.com.br',
-  database: 'booking',
-  password: 'f762e5a29808783a5622',
-  port: 5432
-});
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
 module.exports = pool;
